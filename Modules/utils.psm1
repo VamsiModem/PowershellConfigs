502b03d73acff56ae795fd7547787526ff702d2e
Vamsi Modem <modem.vamsi552@gmail.com>
2021-05-12 09:24:26 -0700

    Added Modules

---
+++ Modules/utils.psm1
@@ -0,0 +1,20 @@
+﻿function AZToken {
+[alias("azt")]
+    param (
+        [Parameter(Mandatory=$false,
+                    HelpMessage="Error from computer.")] 
+        [switch]$graph
+    )
+    BEGIN {
+        if ($graph){
+            $graphToken = az account get-access-token --resource-type ms-graph | ConvertFrom-Json
+            $graphToken.accessToken | Set-Clipboard
+            Write-Output "Copied graph token to clipboard!"
+        } else {
+            $token = az account get-access-token | ConvertFrom-Json
+            $token.accessToken | Set-Clipboard
+            Write-Output "Copied access token to clipboard!"
+        }
+    }
+}
+Export-ModuleMember -Function AZToken
